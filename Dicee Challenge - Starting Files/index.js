c7ccdc0bf7cf2bd88aaeee957b0d0dac3d34d674
ryoya <ryouya1998.1.21@gmail.com>
2022-11-27 14:12:13 +0900

    Completed to Section 13

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,14 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png")
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png")
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player1 win !"
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player2 win !🚩"
+}else{
+    document.querySelector("h1").innerHTML = "🚩Draw🚩"
+}
