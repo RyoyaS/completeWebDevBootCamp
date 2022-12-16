ac79b72b735a334e8c9af9fdec4d8550ec687cb6
RyoyaS <ryouya1998.1.21@gmail.com>
2022-12-16 20:31:41 +0900

    start weather project

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const https = require("https");
+
 const app = express();
 
-app.get("/",(req,res)=>{
+app.get("/",function(req,res){
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=c3d9fc53ceb6aee6bd20a00d8ee3aab5&units=metric"
+    https.get(url,function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+        })
+    })
+
     res.send("Server up and running");
 })
 
