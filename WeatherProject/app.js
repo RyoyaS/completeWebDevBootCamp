f6ec597dbb79202a647f0c1dfd00c245c6346aaf
ryoya <ryouya1998.1.21@gmail.com>
2022-12-17 23:20:35 +0900

    write temp, description

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/",function(req,res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=c3d9fc53ceb6aee6bd20a00d8ee3aab5&units=metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=c3d9fc53ceb6aee6bd20a00d8ee3aab5&units=metric"
     https.get(url,function(response){
         console.log(response.statusCode);
 
@@ -14,10 +14,15 @@ app.get("/",function(req,res){
 
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+
+            res.write("<p>The weather is currently " + description + "</p>");
+            res.write("<h1>The temperature in London is " + temp + " degrees Celsius.</h1>");
+            res.write("<img src="+ imageURL +">");
+            res.send();
         })
     })
-
-    res.send("Server up and running");
 })
 
 app.listen(3000, function(){
